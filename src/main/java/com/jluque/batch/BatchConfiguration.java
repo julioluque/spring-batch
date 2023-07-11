ef7e6391174190c24e65a0b3299718847c4074ae
Julio <44620800+julioluque@users.noreply.github.com>
2023-07-11 15:55:44 -0300

    procesos batch con java 8

--- src/main/java/com/jluque/batch/BatchConfiguration.java
+++ src/main/java/com/jluque/batch/BatchConfiguration.java
@@ -1,51 +1,83 @@
 package com.jluque.batch;
 
+
+import javax.sql.DataSource;
+
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import com.jluque.batch.listener.JobListener;
+import com.jluque.batch.model.Persona;
+import com.jluque.batch.processor.PersonaItemProcessor;
 
 @Configuration
 @EnableBatchProcessing
 public class BatchConfiguration {
 
-    private final JobBuilderFactory jobBuilderFactory;
-    private final StepBuilderFactory stepBuilderFactory;
-    private final DatabaseItemReader databaseItemReader;
-    private final CsvFileItemWriter csvFileItemWriter;
+    @Autowired
+    public JobBuilderFactory jobBuilderFactory;
 
     @Autowired
-    public BatchConfiguration(JobBuilderFactory jobBuilderFactory,
-                              StepBuilderFactory stepBuilderFactory,
-                              DatabaseItemReader databaseItemReader,
-                              CsvFileItemWriter csvFileItemWriter) {
-        this.jobBuilderFactory = jobBuilderFactory;
-        this.stepBuilderFactory = stepBuilderFactory;
-        this.databaseItemReader = databaseItemReader;
-        this.csvFileItemWriter = csvFileItemWriter;
-    }
+    public StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public Step myStep() {
-        return stepBuilderFactory.get("myStep")
-                .<Customer, Customer>chunk(10)
-                .reader(databaseItemReader)
-                .writer(csvFileItemWriter)
+    public FlatFileItemReader<Persona> reader(){
+        return new FlatFileItemReaderBuilder<Persona>()
+                .name("personaItemReader")
+                .resource(new ClassPathResource("sample-data.csv"))
+                .delimited()
+                .names(new String[] {"primerNombre", "segundoNombre", "telefono"})
+                .fieldSetMapper(new BeanWrapperFieldSetMapper<Persona>() {{
+                    setTargetType(Persona.class);
+                }})
                 .build();
     }
 
     @Bean
-    public Job myJob() {
-        return jobBuilderFactory.get("myJob")
+    public PersonaItemProcessor processor() {
+        return new PersonaItemProcessor();
+    }
+
+    @Bean
+    public JdbcBatchItemWriter<Persona> writer(DataSource dataSource){
+        return new JdbcBatchItemWriterBuilder<Persona>()
+                .itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
+                .sql("INSERT INTO persona (primer_nombre, segundo_nombre, telefono) VALUES (:primerNombre, :segundoNombre, :telefono)")
+                .dataSource(dataSource)
+                .build();
+    }
+
+    @Bean
+    public Job importPersonaJob(JobListener listener, Step step1) {
+        return jobBuilderFactory.get("importPersonaJob")
                 .incrementer(new RunIdIncrementer())
-                .flow(myStep())
+                .listener(listener)
+                .flow(step1)
                 .end()
                 .build();
     }
-}
 
+    @Bean
+    public Step step1(JdbcBatchItemWriter<Persona> writer) {
+        return stepBuilderFactory.get("step1")
+                .<Persona, Persona> chunk(10)
+                .reader(reader())
+                .writer(writer)
+                .build();
+    }
+
+}
--- src/main/java/com/jluque/batch/SpringBatchApplication.java
+++ src/main/java/com/jluque/batch/SpringBatchApplication.java
@@ -2,14 +2,12 @@ package com.jluque.batch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-//@ComponentScan(basePackages = "com.jluque.batch.config")
 public class SpringBatchApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBatchApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchApplication.class, args);
+    }
 
 }
